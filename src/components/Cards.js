950b71815985886ea822a4c99b40e5b156484ad3
jooheekim0713 <jooheekim.dev@gmail.com>
2022-08-09 11:51:04 +0900

    fix : Add search queries style : Apply styled-components
    Add search quieries for correct response
    Apply styled-components
    Add messages for no result returns

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,61 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import './Cards.css';
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+
+const Message = styled.h3`
+  color: black;
+`;
+
+const List = styled.ul`
+  margin-top: 10px;
+  padding: 0;
+`;
+const ListItem = styled.li`
+  list-style-type: none;
+  padding: 10px 20px;
+  margin: 4px 0;
+  border: 1px solid black;
+  border-radius: 15px;
+  background-color: #fff;
+`;
+
+const Button = styled.button`
+  background-color: transparent;
+  border: none;
+  float: right;
+  padding: 0;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const Cards = ({ repos }) => {
-  //PAGINATION이 안되기때문에 임시로 SLICE(0,10)
+  const [cards, setCards] = useState([]);
+  let data = repos.data.items;
+  useEffect(() => {
+    if (data.length !== 0) {
+      setCards(data);
+    }
+  }, [data]);
+
   return (
-    <ul>
-      {repos.slice(0, 10).map((repo) => (
-        <li key={repo.id}>
-          {repo.full_name}
-          <button>
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      {data.length === 0 ? (
+        <Message>NO RESULT</Message>
+      ) : (
+        <List>
+          {cards.map((card) => (
+            <ListItem key={card.id}>
+              {card.full_name}
+              <Button>
+                <FontAwesomeIcon icon={faPlus} />
+              </Button>
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </>
   );
 };
 
--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,33 @@
 import { Octokit } from '@octokit/core';
 import { useEffect, useState } from 'react';
-import './List.css';
+import styled from 'styled-components';
 import Cards from './Cards';
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+`;
+
+const Container = styled.div``;
+
+const Input = styled.input`
+  width: 300px;
+  height: 30px;
+  border-radius: 15px;
+`;
 
 function List() {
   const searchKey = 'name';
+  const searchRange = 'public';
+  const pageNum = 2;
+  const perPage = 10;
   const [value, setValue] = useState('');
   const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const onChange = (event) => {
     const {
@@ -14,34 +35,36 @@ function List() {
     } = event;
     setValue(value);
   };
+  //value
   useEffect(() => {
     const fetchRepos = async () => {
       const octokit = new Octokit({
-        auth: `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`,
+        auth: '',
       });
       const response = await octokit.request(
-        `GET /search/repositories?q=${value}in:${searchKey}`
+        `GET /search/repositories?q=${value}in:${searchKey}is:${searchRange}&page=${pageNum}&per_page=${perPage}`
       );
-      const data = response.data.items;
-      setRepos(data);
+
+      //결과값이 존재하지 않을때 처리 필요
+      setRepos(response);
+      setLoading(false);
     };
+
     fetchRepos();
   }, [value]);
 
   return (
-    <div className="content-wrapper">
-      <div className="content-box">
-        <input
+    <Wrapper>
+      <Container>
+        <Input
           type="text"
           value={value}
           onChange={onChange}
           placeholder="repository 명을 입력해주세요"
         />
-        <div className="content">
-          <Cards repos={repos} />
-        </div>
-      </div>
-    </div>
+        {loading ? <h3>loading...</h3> : <Cards repos={repos} />}
+      </Container>
+    </Wrapper>
   );
 }
 
