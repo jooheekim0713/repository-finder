6c7d5a162b91972ffe4a7610a33c652d1c59732f
jooheekim0713 <jooheekim.dev@gmail.com>
2022-08-10 16:39:22 +0900

    feat :  Add function to put items to localStorage
    Add functions to put clicked items into localstorage
    Use validations for conditions

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -32,6 +32,7 @@ const Button = styled.button`
 
 const Cards = ({ repos }) => {
   const [cards, setCards] = useState([]);
+  const [items, setItems] = useState([]);
   const data = repos.data.items;
 
   useEffect(() => {
@@ -40,23 +41,51 @@ const Cards = ({ repos }) => {
     }
   }, [data]);
 
+  const addItem = (card) => {
+    const repoOwner = card.owner.login;
+    const repoName = card.name;
+    const itemObj = {
+      owner: repoOwner,
+      name: repoName,
+    };
+    validItems(itemObj, repoOwner, repoName) //
+      .then(localStorage.setItem('items', JSON.stringify(items)));
+  };
+
+  const validItems = async (itemObj, repoOwner, repoName) => {
+    const validItem = items.map(
+      (ele) => repoOwner === ele.owner && repoName === ele.name
+    );
+    if (items.length < 4) {
+      if (validItem.includes(true)) {
+        alert('기존에 등록된 repository입니다.');
+        return;
+      } else {
+        setItems((current) => [...current, itemObj]);
+      }
+    } else {
+      alert(
+        '저장할 수 있는 repo 개수를 초과했습니다.\n(저장할 수 있는 repo 개수는 4개 입니다.)'
+      );
+      return;
+    }
+  };
+
   return (
     <>
       {data.length === 0 ? (
         <Message>NO RESULT</Message>
       ) : (
-        <>
-          <List>
-            {cards.map((card) => (
-              <ListItem key={card.id}>
-                {card.full_name}
-                <Button>
-                  <FontAwesomeIcon icon={faPlus} />
-                </Button>
-              </ListItem>
-            ))}
-          </List>
-        </>
+        <List>
+          {cards.map((card) => (
+            <ListItem key={card.id}>
+              {card.full_name}
+              <Button onClick={() => addItem(card)}>
+                <FontAwesomeIcon icon={faPlus} />
+              </Button>
+            </ListItem>
+          ))}
+        </List>
       )}
     </>
   );
