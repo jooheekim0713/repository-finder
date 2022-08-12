1aafce521e69d1aace9d46fe676bb7a110ccbcf1
jooheekim0713 <jooheekim.dev@gmail.com>
2022-08-12 21:38:02 +0900

    fix : Change url

--- src/Router.js
+++ src/Router.js
@@ -7,7 +7,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/issues" element={<Issues />} />
+        <Route path="/issues/:owner/:name" element={<Issues />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/" element={<List />} />
       </Routes>
